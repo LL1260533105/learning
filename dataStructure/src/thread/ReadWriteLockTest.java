7f3078c00195c2b0b84603a5f444b36021664d99
Mr.Li <7704195+Mlijilin@user.noreply.gitee.com>
2021-01-05 23:02:11 +0800

    多线程实践：ReentrantLock，ReadWriteLock，CountDownLatch 实验demo

---
+++ dataStructure/src/thread/ReadWriteLockTest.java
@@ -0,0 +1,69 @@
+package thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ *  读写锁测试
+ *  读锁 共享锁  可以同时又多个线程并行读
+ *  写锁 排他锁  每次只能有一个线程进行写操作，并且当有线程正在读时，
+ *              也是不能进行写操作的，等到所有的读或者写操作完成后，下一个写操作才能进行写操作
+ */
+public class ReadWriteLockTest {
+
+    /**
+     * 读写锁
+     */
+    public ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    /**
+     * 读锁
+     */
+    public Lock readLock = readWriteLock.readLock();
+
+    /**
+     * 写锁
+     */
+    public Lock writeLock = readWriteLock.writeLock();
+
+    public void read(){
+        try {
+            readLock.lock();
+            System.out.println(Thread.currentThread().getName() + "  正在读");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public void write(){
+        try {
+            writeLock.lock();
+            System.out.println(Thread.currentThread().getName() + "  正在写");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(Thread.currentThread().getName() + "  写完成");
+            writeLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReadWriteLockTest readWriteLockTest = new ReadWriteLockTest();
+        for(int i = 0; i <10; i++){
+            new Thread(() -> {
+                readWriteLockTest.read();
+            }).start();
+        }
+
+        for(int i = 0; i <3; i++){
+            new Thread(() -> {
+                readWriteLockTest.write();
+            }).start();
+        }
+    }
+}
---
+++ dataStructure/src/thread/ReentrantLockTest.java
@@ -0,0 +1,39 @@
+package thread;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * ReentrantLock 测试
+ * 如下小程序 如果使用 ReentrantLock 加锁 ，main函数输出一定是start，然后end ，加上线程名一组一组的输出
+ *           如果不加锁，两个线程输出则为start一起输出，然后输出end
+ */
+public class ReentrantLockTest {
+
+    public ReentrantLock reentrantLock = new ReentrantLock();
+
+    public void lockTest(){
+        try {
+            // 手动加锁
+            reentrantLock.lock();
+            System.out.println("test start:" + Thread.currentThread().getName());
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // 手动释放锁
+            // 注意加锁后相应的业务执行完后，一定要释放锁
+            reentrantLock.unlock();
+            System.out.println("test end:" + Thread.currentThread().getName());
+        }
+    }
+    public static void main(String[] args) {
+        ReentrantLockTest test = new ReentrantLockTest();
+        new Thread(() -> {
+            test.lockTest();
+        }).start();
+
+        new Thread(() -> {
+            test.lockTest();
+        }).start();
+    }
+}
